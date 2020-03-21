9546a6ee815a064f5758c47bc35af93e60e56f4d
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-03-21 19:07:13 +0900

    1章のBuilding with Component まで

---
+++ hello-world/src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="about gatsby" />
+    <Header headerText="aaaa" />
+    <p>such</p>
+  </div>
+)
---
+++ hello-world/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello gatsby!</h1>
+    <p>What a world</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
