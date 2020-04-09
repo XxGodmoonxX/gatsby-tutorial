9b2256935392a47a801b0fb5a4285705b9a03410
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-04-10 01:18:04 +0900

    1章 Linkking Between Linkking Between Pagesまで

--- hello-world/src/pages/index.jsx
+++ hello-world/src/pages/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 export default () => (
   <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
     <h1>Hello gatsby!</h1>
     <p>What a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
