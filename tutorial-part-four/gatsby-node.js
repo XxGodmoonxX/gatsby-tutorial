f82d2e08e97a7b308e128b84e5e7a9f021d29807
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-05-09 22:18:10 +0900

    create pages with markdown

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -27,5 +28,14 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  console.log(JSON.stringify(result, null, 4))
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.jsx`),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
